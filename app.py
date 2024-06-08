1c7008f9bd3e58d2279ea2c97f6fa6d91011ffd8
Sourabh-Pujari25 <sourabhpujari2599@gmail.com>
2024-06-08 14:06:13 +0530

    trash

--- app.py
+++ app.py
@@ -7,48 +7,50 @@ st.set_page_config(page_title='Oliots ERP', page_icon='images/logo.ico')
 #078a97
 
 def main():
-    
-    st.markdown('''<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Image Visibility</title>
-    <style>
-        /* Default style: hide the image */
-        .mobile-only {
-            display: none;
-        }
-
-        /* Media query for screens with max-width 768px (typical for mobile devices) */
-        @media only screen and (max-width: 768px) {
-            .mobile-only {
-                display: block; /* Show the image */
-            }
-        }
-    </style>
-</head>
-<body>
-    <img class="mobile-only" src="your-image-url.jpg" alt="Mobile-only image">
-</body>
-</html>
-
-    
-''',unsafe_allow_html=True)
-    
-
-    # Sidebar 
-    st.sidebar.markdown(f"""<style>[class="st-emotion-cache-1gv3huu eczjsme16"]{{
-   background: linear-gradient(to bottom right, #053c47, #078a97);}}</style>""",unsafe_allow_html=True)
-    #collapse button
-    st.sidebar.markdown(f"""<style>[class="eyeqlp51 st-emotion-cache-1pbsqtx ex0cdmw0"]{{display: none;}}</style>""",unsafe_allow_html=True)
     image_path = f"{IMAGES}{LOGO_IMAGE}"
-    st.sidebar.markdown("""<div style="
-    height: 170px;
-"></div>""",unsafe_allow_html=True)
+    
+#     st.markdown(f'''<!DOCTYPE html>
+# <html lang="en">
+# <head>
+#     <meta charset="UTF-8">
+#     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+#     <title>Image Visibility</title>
+#     <style>
+#         /* Default style: hide the image */
+#         .mobile-only {{
+#             display: none;
+#         }}
+
+#         /* Media query for screens with max-width 768px (typical for mobile devices) */
+#         @media only screen and (max-width: 768px) {{
+#             .mobile-only {{
+#                 display: block; /* Show the image */
+#             }}
+#         }}
+#     </style>
+# </head>
+# <body>
+#     <img class="mobile-only" src="{image_path}" alt="Mobile-only image">
+                
+# </body>
+# </html>
+
+    
+# ''',unsafe_allow_html=True)
+    
+
+#     # Sidebar 
+#     st.sidebar.markdown(f"""<style>[class="st-emotion-cache-1gv3huu eczjsme16"]{{
+#    background: linear-gradient(to bottom right, #053c47, #078a97);}}</style>""",unsafe_allow_html=True)
+#     #collapse button
+#     st.sidebar.markdown(f"""<style>[class="eyeqlp51 st-emotion-cache-1pbsqtx ex0cdmw0"]{{display: none;}}</style>""",unsafe_allow_html=True)
+#     image_path = f"{IMAGES}{LOGO_IMAGE}"
+#     st.sidebar.markdown("""<div style="
+#     height: 170px;
+# "></div>""",unsafe_allow_html=True)
     st.sidebar.image(image_path) 
     #containers
-    st.sidebar.markdown(f"""<style>[class="st-emotion-cache-r421ms e1f1d6gn0"]{{padding: 50px;}}</style>""",unsafe_allow_html=True)
+    # st.sidebar.markdown(f"""<style>[class="st-emotion-cache-r421ms e1f1d6gn0"]{{padding: 50px;}}</style>""",unsafe_allow_html=True)
 
     
     

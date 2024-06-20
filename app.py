f1f332349322b2b89e70fde15a9528fb72061b0f
Sourabh-Pujari25 <sourabhpujari2599@gmail.com>
2024-06-20 20:34:46 +0530

    UI

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from app_utils import *
 from streamlit_option_menu import option_menu
 import base64
 st.set_page_config(page_title='Oliots ERP', page_icon='images/logo.ico',layout="wide")
-#078a97
-#@st.experimental_memo
+
 def get_img_as_base64(file):
     with open(file, "rb") as f:
         data = f.read()
@@ -20,7 +19,7 @@ background-repeat: no-repeat;
 background-attachment: fixed;
 }}</style>""",unsafe_allow_html=True)
 
-#st.set_page_config(page_title='Oliots ERP', page_icon='images/logo.ico',layout="wide")
+
 def main():
     # Sidebar hide
     
@@ -28,8 +27,6 @@ def main():
     #topbar
     st.markdown(f"""<style>[class="st-emotion-cache-12fmjuu ezrtsby2"]{{display:none;}}</style>""",unsafe_allow_html=True)
     #pages  
-    # st.markdown(f"""<style>[class="st-emotion-cache-10rjk4g eczjsme14"]{{display:none; }}</style>""",unsafe_allow_html=True)
-    # st.markdown(f"""<style>[class="st-emotion-cache-1mi2ry5 eczjsme6"]{{display:none; }}</style>""",unsafe_allow_html=True)
     st.markdown(f"""<style>[class="eyeqlp51 st-emotion-cache-1f3w014 ex0cdmw0"]{{display:none; }}</style>""",unsafe_allow_html=True)
     #image expander 
     st.markdown(f"""<style>[data-testid="StyledFullScreenButton"]{{display:none; }}</style>""",unsafe_allow_html=True)
@@ -42,66 +39,8 @@ def main():
     image_path = f"{IMAGES}{LOGO_IMAGE}"
     col1,space,col2=st.columns([30,10,60])
     with col1:
-        css = """
-                <style>
-                /* Hide the div on screens smaller than 768px */
-                @media only screen and (max-width: 768px) {
-                    .desktop-only {
-                        display: none;
-                        
-                    }
-                }
-
-                /* Show the div on screens larger than or equal to 768px */
-                @media only screen and (min-width: 768px) {
-                    .desktop-only {
-                        display: block;
-                        height:120px
-                    }
-                }
-                </style>
-                """
-        html = """
-            <div class="desktop-only">
-            <p>--</p>
-                
-            </div>
-            """
-        st.markdown(css + html, unsafe_allow_html=True)
-
-
-
-#         st.markdown("""<style>
-#         .responsive-image {
-#             height: 120px; /* Show by default */
-#         }
-        
-#         @media (max-width: 768px) {
-#             .responsive-image {
-#                 margin-top: -140px; /* Hide on small screens */
-#             }
-#         }
-#     </style>
-# </head>
-# <body>
-#                     <div class="responsive-image"></div>
-    
-# </body>""",unsafe_allow_html=True)
-
-
-
-
-
-
-
-        #st.markdown("""<div style="height: 120px;"></div>""",unsafe_allow_html=True)
         st.image(image_path,use_column_width=True) 
-    with col2:
-
-    
-   
-    #st.sidebar.markdown("""<div style="height: 250px;"></div>""",unsafe_allow_html=True)
-    
+    with col2:    
         space1,logo_space,space2=st.columns([20,60,20])
         with logo_space:
             st.markdown("""<center><h1 style="color: teal;">Sign in to Workspace</h1></center>""",unsafe_allow_html=True)
